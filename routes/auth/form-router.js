f63b77b9ba89de0567da6864a0ae7638ebdd1f3e
Leejunheezz123 <dotm3434@naver.com>
2021-12-09 11:16:27 +0900

    MVC 패턴 적용 시작

--- routes/auth/form-router.js
+++ routes/auth/form-router.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
-const { error } = require('../../modules/util-module')
-const { pool } = require('../../modules/mysql-module')
+const createError = require('http-errors')
+const { alert } = require('../../modules/util-module')
+const {createUser} = require('../../models')
 
 router.get('/',(req, res, next) => {
     //join 창 보여주기
@@ -12,8 +13,18 @@ router.get('/',(req, res, next) => {
     const info = null
     res.status(200).render('auth/form',{   js, css,info   })
 })
-router.post('/',(req, res, next) => {
+router.post('/',async (req, res, next) => {
     //실제 join 처리
+    try{
+        const r = await createUser(req.body)
+        if(r.success) res.redirect('/')
+        else if(r.msg) res.send(alert(msg))
+        else next(createError(r.err))
+
+    }
+    catch(err){
+        next(createError(err))
+    }
 })
 
 module.exports = router 
