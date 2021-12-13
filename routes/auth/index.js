b4330fe3d12b65da97b271822131a20c399b43c7
Leejunheezz123 <dotm3434@naver.com>
2021-12-13 23:08:53 +0900

    kakao 로그인창 띄우기 완료

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -5,11 +5,15 @@ const { error } = require('../../modules/util-module')
 const { pool } = require('../../modules/mysql-module')
 const loginRouter = require('./login-router')
 const logoutRouter = require('./logout-router')
+const kakaoRouter = require('./kakao-router.js')
+const naverRouter = require('./naver-router')
 const formRouter = require('./form-router')
 
 
 router.use('/login',loginRouter)
 router.use('/logout',logoutRouter)
+router.use('/kakao',kakaoRouter)
+router.use('/naver',naverRouter)
 router.use('/form',formRouter)
 
 module.exports = router
