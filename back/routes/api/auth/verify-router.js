c71c2d4e602c03494ed06b40759b6d518753254c
Leejunheezz123 <dotm3434@naver.com>
2021-12-21 20:17:56 +0900

    passport naver 원본수정 profile,strategy 두개에서 바꿈.

--- back/routes/api/auth/verify-router.js
+++ back/routes/api/auth/verify-router.js
@@ -2,18 +2,25 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const { error } = require('../../../modules/util-module')
-const { mysql,pool } = require('../../../modules/mysql-module')
-const {existUser} = require('../../../models/auth')
+const { mysql, pool } = require('../../../modules/mysql-module')
+const { existUser } = require('../../../models/auth')
 
-router.get('/verify',async(req, res, next) => {
-    // userid, email 중복 검증
-    try{
-        const {success:isUsed} = await existUser(req.query.key, req.query.value)
-        res.status(200).json({isUsed})
-    }
-    catch(err){
-        next(createError(err))
-    }
+router.get('/verify', async (req, res, next) => {
+	// userid, email 중복 검증
+	try {
+		let { key, value, isUsed } = req.query
+		if(req.user && key === 'email' && req.user.email === value) {
+			isUsed = false;
+		}
+		else {
+			const { success } = await existUser(key, value)
+			isUsed = success;
+		}
+		res.status(200).json({ isUsed })
+	}
+	catch(err) {
+		next(createError(err))
+	}
 })
 
-module.exports = router  
+module.exports = router
